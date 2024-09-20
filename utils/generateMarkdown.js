0dca0265ad29c52e473c30c5e1d8b6c80d528fad
ShirishaKb <shirishakb@gmail.com>
2024-09-20 15:03:21 -0400

    modified index.js file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -20,16 +20,16 @@ function renderLicenseBadge(license) {
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   if (license === 'MIT') {
-    return `[MIT](https://opensource.org/licenses/MIT)`;
+    return `[License: MIT](https://opensource.org/licenses/MIT)`;
   }
   if (license === 'GNU GPLv3') {
-    return `[GNU GPLv3](https://www.gnu.org/licenses/gpl-3.0)`;
+    return `[License: GNU GPLv3](https://www.gnu.org/licenses/gpl-3.0)`;
   }
   if (license === 'Apache 2.0') {
-    return `[Apache 2.0](https://opensource.org/licenses/Apache-2.0)`;
+    return `[License: Apache 2.0](https://opensource.org/licenses/Apache-2.0)`;
   }
   if (license === 'ISC') {
-    return `[ISC](https://opensource.org/licenses/ISC)`;
+    return `[License: ISC](https://opensource.org/licenses/ISC)`;
   }
   return '';
 }
@@ -48,30 +48,40 @@ function renderLicenseSection(license) {
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# Title: ${data.title}
   ${renderLicenseBadge(data.license)}
+
           ## Description
           ${data.description}
+
           ## Table of Contents
           * [Installation](#installation)
           * [Usage](#usage)
-          * [License](#license)
           * [Contributing](#contributing)
           * [Tests](#tests)
+          * [License](#license)
           * [Questions](#questions)
+          * [Video](#video)
+          
           ## Installation
           ${data.installation}
+
           ## Usage
           ${data.usage}
+
           ## Contributing
           ${data.contributing}
+
           ## Tests
           ${data.tests}
+
           ## License
           ${renderLicenseSection(data.license)}
+
           ## Questions
           * GitHub: [${data.github}](https://github.com/${data.github})
           * Email: [${data.email}](mailto:${data.email})
+          
           ## Video
           [Click here to view the video]${data.video}
           `;
